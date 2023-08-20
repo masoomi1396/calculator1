277a1a68ef69266a9bde31de79cfa177204cd450
Amirmohammad Masoumi <39368982+masoomi1396@users.noreply.github.com>
2023-08-20 06:23:14 +0330

    Add files via upload

---
+++ assets/Js/master.js
@@ -0,0 +1,101 @@
+let number1 = "";
+let number2 = null;
+let lastAction = "";
+const screen = document.getElementsByClassName("screen")[0];
+function calculator(_key) {
+    switch (_key) {
+        case '+':
+            if (number1 != "") {
+                if (number2 != null) {
+                    number1 = Action(number1, number2, _key);
+                    number2 = null;
+                    lastAction = "";
+                }
+                lastAction = _key;
+                number2 = number1;
+                number1 = "";
+            }
+            screen.innerText = 0;
+            break;
+        case '-':
+            if (number1 != "") {
+                if (number2 != null) {
+                    number1 = Action(number1, number2, _key);
+                    number2 = null;
+                    lastAction = "";
+                }
+                lastAction = _key;
+                number2 = number1;
+                number1 = "";
+            }
+            screen.innerText = 0;
+            break;
+        case '÷':
+            if (number1 != "") {
+                if (number2 != null) {
+                    number1 = Action(number1, number2, _key);
+                    number2 = null;
+                    lastAction = "";
+                }
+                lastAction = _key;
+                number2 = number1;
+                number1 = "";
+            }
+            screen.innerText = 0;
+            break;
+        case 'x':
+            if (number1 != "") {
+                if (number2 != null) {
+                    number1 = Action(number1, number2, _key);
+                    number2 = null;
+                    lastAction = "";
+                }
+                lastAction = _key;
+                number2 = number1;
+                number1 = "";
+            }
+            screen.innerText = 0;
+            break;
+        case '=':
+            if (number1 != "") {
+                if (number2 != null) {
+                    number1 = Action(number1, number2, lastAction);
+                    number2 = null;
+                    lastAction = _key;
+                }
+                else {
+                    alert("Please Enter Two Numbers");
+                }
+            }
+            screen.innerText = number1;
+            break;
+        default:
+            if(lastAction == "=")
+            {
+                if(window.confirm("You Are Rewrite your Result!! Are you sure ??"))
+                    lastAction = "";
+                break;
+            }
+            number1 += _key;
+            screen.innerText = number1;
+            break;
+    }
+}
+function Clear() {
+    number1 = "";
+    number2 = null;
+    lastAction = "";
+    screen.innerText = 0;
+}
+function Action(number1, number2, action) {
+    if (action == '+')
+        return Number(number1) + Number(number2);
+    else if (action == '-')
+        return Number(number2) - Number(number1);
+    else if (action == '÷')
+        return Number(number2) / Number(number1);
+    else {
+        console.log("Action : " + action);
+        return Number(number1) * Number(number2);
+    }
+}
